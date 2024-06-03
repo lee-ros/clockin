1a85cfa2b702cf6eb7cd7674f5cc7618d3cd8184
Lee Rosenberg <laluk321@gmail.com>
2024-06-03 13:32:15 -0400

    add users repo

---
+++ packages/server/src/db/repos/users.ts
@@ -0,0 +1,29 @@
+import { IDatabase, IMain } from "pg-promise";
+import { users as sql } from "../sql";
+import { User } from "../../types/users";
+import { UUID } from "crypto";
+
+export class UsersRepository {
+  constructor(private db: IDatabase<any>, private pgp: IMain) {}
+
+  create(): Promise<null> {
+    return this.db.none(sql.createTable);
+  }
+
+  drop(): Promise<null> {
+    return this.db.none("DROP TABLE users");
+  }
+
+  add(values: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  }): Promise<User> {
+    return this.db.one(sql.add, values);
+  }
+
+  delete(values: { id: UUID }): Promise<null> {
+    return this.db.none(sql.delete, values.id)
+  }
+}
