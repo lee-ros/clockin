904417a22c839e84d23c8cae0caa321c06ce64ed
Lee Rosenberg <laluk321@gmail.com>
2024-06-03 13:35:53 -0400

    add routes to app

--- packages/server/src/createApp.ts
+++ packages/server/src/createApp.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { Express } from "express-serve-static-core";
-import taskRouter from './routes/tasks';
+import tasksRouter from "./routes/tasks";
+import usersRouter from './routes/users';
 
 export default function createApp(): Express {
   const app = express();
 
   app.use(express.json());
 
-  app.use("/api/tasks", taskRouter);
+  app.use("/api/tasks", tasksRouter);
+  app.use("/api/users", usersRouter);
 
   return app;
 }
