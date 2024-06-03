fcc22f1803c2317d0d10913d9462dc1ba6cc286f
Lee Rosenberg <laluk321@gmail.com>
2024-06-03 13:32:45 -0400

    add users repo to the db object

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,10 +1,21 @@
-import { IDatabase, IMain } from "pg-promise";
+import { IDatabase, IInitOptions, IMain } from "pg-promise";
+import { IExtentions } from "./repos";
+import { UsersRepository } from "./repos";
 
-const pgp: IMain = require("pg-promise")();
+type ExtendedProtocol = IDatabase<IExtentions> & IExtentions;
+
+const initOptions: IInitOptions<IExtentions> = {
+  extend(obj, dc: any) {
+    obj.users = new UsersRepository(obj, pgp);
+  },
+};
+
+const pgp: IMain = require("pg-promise")(initOptions);
+
 console.log(process.env.DB_URL);
 if (!(typeof process.env.DB_URL === "string")) {
-    throw Error("No Database URL found in the environment");
+  throw Error("No Database URL found in the environment");
 }
 
-const db = pgp(process.env.DB_URL);
+const db: ExtendedProtocol = pgp(process.env.DB_URL);
 export { db, pgp };
